ff7b5f7becd195ab83a9597613a62733d4b96ee8
Richard Walker <richard.walker.90@gmail.com>
2017-10-14 13:15:51 -0400

    refactored into functions

--- js/example.js
+++ js/example.js
@@ -5,15 +5,16 @@ let message = '';
 
 const addition = (number1, number2) => number1 + number2; 
 const multiply = (number1, number2) => number1 * number2; 
+const createMessage = (number1, number2, operation) => number1 + ' + ' + number2 + ' = ' + operation + '<br/>';
 
 if(operator === 'addition'){
     while (i < 11){
-        message += i + ' + ' + table + ' = ' + addition(table, i) + '<br/>';
+        message += createMessage(i, table, addition(table, i));
         i++;
     }
 } else {
     while (i < 11){
-        message += i + ' * ' + table + ' = ' + multiply(table, i) + '<br/>';
+        message += createMessage(i, table, multiply(table, i));;
         i++;
     }
 }
